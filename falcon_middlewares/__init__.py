89fd809c14678c2c1e7846fbd87adb39bdc745c9
MoisesFlores22 <moises.flores@wizeline.com>
2017-10-16 19:00:07 -0500

    Fix error

--- falcon_middlewares/__init__.py
+++ falcon_middlewares/__init__.py
@@ -2,4 +2,4 @@
 __version__ = '1.0.0'
 
 from falcon_middlewares.client_secret import ClientSecretMiddleware
-from falcon_middlewares.client_secret import JSONMiddleware
+from falcon_middlewares.json import JSONMiddleware
