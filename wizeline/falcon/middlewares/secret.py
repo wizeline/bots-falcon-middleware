9c6e6cca81bd81e7ad4dcb8901790e02a618cf52
MoisesFlores22 <moises.flores@wizeline.com>
2017-10-19 14:04:43 -0500

    Change secret middleware

--- wizeline/falcon/middlewares/secret.py
+++ wizeline/falcon/middlewares/secret.py
@@ -3,16 +3,16 @@ import falcon
 
 def require_secret(req, resp, resource, params):
     secret = req.get_header('Authorization')
-
     if not req._secret == secret:
         raise falcon.HTTPUnauthorized
 
-class APISecretMiddleware: 
-    def __init__(self, secret):
+
+class APISecretMiddleware:
+    def __init__(self, secret, required=True):
         self._secret = secret
-    
-    def process_resource(self, req, resp, resource, params, required):
-        if require:
-            if not req.get_header('Authorization') == self._secret:
-              raise falcon.HTTPUnauthorized
+        self.required = required
+
+    def process_resource(self, req, resp, resource, params):
+        if self.required and not req.get_header('Authorization') == self._secret:
+            raise falcon.HTTPUnauthorized
         req._secret = self._secret
