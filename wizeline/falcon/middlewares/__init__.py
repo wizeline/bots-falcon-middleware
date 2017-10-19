27ee082d67109ae8bf2b0af6dcbc25f6452f0062
MoisesFlores22 <moises.flores@wizeline.com>
2017-10-19 11:23:15 -0500

    Make changes to the secret middleware to make it more readable

--- wizeline/falcon/middlewares/__init__.py
+++ wizeline/falcon/middlewares/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
 __version__ = '1.0.0'
 
-from wizeline.falcon.middlewares.secret import SecretMiddleware, AlwayRequireSecretMiddleware
+from wizeline.falcon.middlewares.secret import APISecretMiddleware
 from wizeline.falcon.middlewares.json import JSONMiddleware
--- wizeline/falcon/middlewares/secret.py
+++ wizeline/falcon/middlewares/secret.py
@@ -1,24 +1,18 @@
 import falcon
 
 
-class SecretMiddleware: 
+def require_secret(req, resp, resource, params):
+    secret = req.get_header('Authorization')
+
+    if not req._secret == secret:
+        raise falcon.HTTPUnauthorized
+
+class APISecretMiddleware: 
     def __init__(self, secret):
         self._secret = secret
     
-    def process_resource(self, req, resp, resource, params):
+    def process_resource(self, req, resp, resource, params, required):
+        if require:
+            if not req.get_header('Authorization') == self._secret:
+              raise falcon.HTTPUnauthorized
         req._secret = self._secret
-    
-    def require_secret(req, resp, resource, params):
-        secret = req.get_header('Authorization')
-
-        if not req._secret == secret:
-            raise falcon.HTTPUnauthorized
-
-
-class AlwayRequireSecretMiddleware:
-    def __init__(self, secret):
-        self._secret = secret
-
-    def process_resource(self, req, resp, resource, params):
-        if not req.get_header('Authorization') == self._secret:
-            raise falcon.HTTPUnauthorized
