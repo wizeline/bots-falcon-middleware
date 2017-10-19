f1dc441dc7b611b4f4790b288f6ea62947c6d67c
MoisesFlores22 <moises.flores@wizeline.com>
2017-10-19 11:23:33 -0500

    Import require secret

--- wizeline/falcon/middlewares/__init__.py
+++ wizeline/falcon/middlewares/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
 __version__ = '1.0.0'
 
-from wizeline.falcon.middlewares.secret import APISecretMiddleware
+from wizeline.falcon.middlewares.secret import APISecretMiddleware, require_secret
 from wizeline.falcon.middlewares.json import JSONMiddleware
