a2756768619395e21f2d11afa6f0ff3322fb7710
MoisesFlores22 <moises.flores@wizeline.com>
2017-10-19 17:05:38 -0500

    Simplify things

--- wizeline/falcon/middlewares/secret.py
+++ wizeline/falcon/middlewares/secret.py
@@ -9,21 +9,13 @@ def require_secret(req, resp, resource, params):
 
 class APISecretMiddleware:
     def __init__(self, secret, required=True):
-        print(required)
         self._secret = secret
-        self._required = required
-    
-    def _is_secret_required(self):
-        if self._required:
-            return True
-        return False
+        self._is_secret_required = required
 
     def _has_valid_secret(self, req):
-        if not req.get_header('Authorization') == self._secret:
-            return True
-        return False
+        return req.get_header('Authorization') == self._secret
 
     def process_resource(self, req, resp, resource, params):
-        if self._is_secret_required() and self._has_valid_secret(req):
+        if self._is_secret_required and not self._has_valid_secret(req):
             raise falcon.HTTPUnauthorized
         req._secret = self._secret
